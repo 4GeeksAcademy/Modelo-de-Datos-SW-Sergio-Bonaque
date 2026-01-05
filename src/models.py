ffc982a5cb5b5fdab290e908cce8318d582c79f8
Sergio Bonaque <s.bonaquecontacto@gmail.com>
2026-01-05 17:49:56 +0000

    correccion

--- src/models.py
+++ src/models.py
@@ -9,3 +9,14 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+ 
+    # Relación con favoritos
+    favorites = relationship("Favorite", back_populates="user")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
